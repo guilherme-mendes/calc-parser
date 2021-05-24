a2003d143e288955361c218a724415c93d36347c
Guilherme Mendes <guimendesp12@gmail.com>
2021-05-23 22:55:47 -0300

    Add parser and change user data

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "Guilherme Mendes Pereira"
+matricula = "17/0129411"
+email = "guimendesp12@gmail.com"
+usuario_github = "guilherme-mendes"
